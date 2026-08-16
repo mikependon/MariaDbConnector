a191b2bd938cf7ce93a42b05c5697b1540110baa
Michael Camara Pendon <michael.c.pendon@outlook.com>
2026-08-16 13:28:54 +0200

    Added the XML documentation strings.

--- src/MariaDbConnector/MariaDbConnector/MariaDbProviderFactory.cs
+++ src/MariaDbConnector/MariaDbConnector/MariaDbProviderFactory.cs
@@ -2,25 +2,43 @@ using System.Data.Common;
 
 namespace MariaDbConnector
 {
+    /// <summary>
+    /// Represents a set of methods for creating instances of the MariaDB client implementation of the data source classes.
+    /// </summary>
     public class MariaDbProviderFactory : DbProviderFactory
     {
+        /// <summary>
+        /// Gets an instance of the <see cref="MariaDbProviderFactory"/>.
+        /// </summary>
         public static readonly MariaDbProviderFactory Instance = new MariaDbProviderFactory();
 
+        /// <summary>
+        /// Returns a strongly typed <see cref="DbConnection"/> instance.
+        /// </summary>
         public override DbConnection CreateConnection()
         {
             return new MariaDbConnection();
         }
 
+        /// <summary>
+        /// Returns a strongly typed <see cref="DbCommand"/> instance.
+        /// </summary>
         public override DbCommand CreateCommand()
         {
             return new MariaDbCommand();
         }
 
+        /// <summary>
+        /// Returns a strongly typed <see cref="DbParameter"/> instance.
+        /// </summary>
         public override DbParameter CreateParameter()
         {
             return new MariaDbParameter();
         }
 
+        /// <summary>
+        /// Returns a strongly typed <see cref="DbConnectionStringBuilder"/> instance.
+        /// </summary>
         public override DbConnectionStringBuilder CreateConnectionStringBuilder()
         {
             return new MariaDbConnectionStringBuilder();
